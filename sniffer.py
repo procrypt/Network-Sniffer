63cbb2e1d6916a64889f6ea64f300d09689cd4bc
Abhishek Pratap Singh <abhishek1004@outlook.com>
2015-03-27 21:33:44 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -12,6 +12,7 @@
 
      You should have received a copy of the GNU General Public License
      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     Name : Network Sniffer v1.0 Beta 
      Coder : Abhishek Pratap Singh
 '''
 
